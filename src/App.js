febde78edc411601723d6da4c361f435a5970c0c
Brandon328 <70345941@continental.edu.pe>
2023-03-24 12:15:40 -0500

    maquetacion y creación de componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React from 'react';
+import { TodoCounter } from './components/TodoCounter';
+import { TodoSearch } from './components/TodoSearch';
+import { TodoList } from './components/TodoList';
+import { CreateTodoButton } from './components/CreateTodoButton';
+import { TodoItem } from './components/TodoItem';
+
+const todos = [
+  {
+    text: 'Cortar Cebolla',
+    completed: false
+  },
+  {
+    text: 'Tomar el curso de intro a React',
+    completed: false
+  },
+  {
+    text: 'Llorar con la llorona',
+    completed: false
+  },
+]
 
 // Componente
 function App() {
+  // React solo nos permite enviar una sola etiqueta
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Platzi
-        </a>
-      </header>
-    </div>
+    // React.Fragment es una etiqueta invisible para evitar los div's innecesarios
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {
+          todos.map((todo, index) => (
+            <TodoItem key={index} text={todo.text} />
+          ))
+        }
+      </TodoList>
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
